e5e87e84b06ab3b8c22ecc554d41585601ff8b97
MarijnSt <marijn.stammeleer@gmail.com>
2019-05-20 11:00:22 +0200

    working loop

--- src/loops.php
+++ src/loops.php
@@ -4,8 +4,31 @@
     <title>Looping php</title>
   </head>
   <body>
-    <?php
-      echo "start looping";
-    ?>
+    <h1>TV shows</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>TV show</th>
+          <th>Rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $shows = [
+            ["tv-show" => "Peaky Blinders", "rating" => 5],
+            ["tv-show" => "Breaking Bad", "rating" => 5],
+            ["tv-show" => "Game Of Thrones", "rating" => 0],
+            ["tv-show" => "South Park", "rating" => 4]
+          ];
+        ?>
+
+        <?php foreach($shows as $arr): ?>
+        <tr>
+        	<td><?php echo $arr["tv-show"]; ?></td>
+        	<td><?php echo $arr["rating"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
   </body>
 </html>
